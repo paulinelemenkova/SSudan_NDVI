07b9bab42bf06b4342fe1c90a6e37c3e7db3badb
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2023-07-15 20:24:14 +0200

    Add files via upload

---
+++ R-terra_NDVI_SSudan_2019.r
@@ -0,0 +1,27 @@
+# Computing vegetation indices
+library(terra)
+library(RColorBrewer)
+setwd("/Users/polinalemenkova/Documents/R/52_Image_Processing/NDVI")
+library(Hmisc)
+library(pals)
+# 1. Normalized Difference Vegetation Index (NDVI) = (NIR - R) / (NIR + R), i.e., NDVI = (Band 5 – Band 4) / (Band 5 + Band 4).
+vi <- function(img, k, i) {
+  bk <- img[[k]]
+  bi <- img[[i]]
+  vi <- (bk - bi) / (bk + bi)
+  return(vi)
+}
+# For Landsat NIR = 5, red = 4.
+filenames <- paste0('LC08_L2SP_173055_20190308_20200829_02_T1_SR_B', 1:7, ".tif")
+filenames
+landsat <- rast(filenames)
+landsat
+ndvi <- vi(landsat, 5, 4)
+plot(ndvi, col=brewer.pal(11, "RdYlGn"), axes=FALSE, ann=FALSE)
+
+# Plotting histogram of the NDVI
+hist(ndvi, font.main = 1, main = "NDVI values for Landsat-8 OLI/TIRS C1 image \nLC08_L2SP_197050_20131110_20200912_02_T1_SR: Inner Niger Delta, Mali (2013)", xlab = "NDVI", ylab= "Frequency",
+    col = "darkolivegreen1", xlim = c(-0.5, 1),  breaks = 30, xaxt = "n")
+axis(side=1, at = seq(-0.6, 1, 0.1), labels = seq(-0.6, 1, 0.1))
+minor.tick(nx = 10, ny = 10, tick.ratio = 0.3)
+#
